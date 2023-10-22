d03afdfb8f7a87850d3fa2b6faf0777558fe58a0
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2023-10-22 17:03:55 +0200

    Перевел blogRepo к наследованию от MongooseRepo

--- src/Entities/Repos/BlogsRepo/blogsRepo.service.ts
+++ src/Entities/Repos/BlogsRepo/blogsRepo.service.ts
@@ -3,36 +3,11 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Blog, BlogDocument } from './Schemas/blog.schema';
 import { CreateBlogDto } from './Dtos/CreateBlogDto';
+import { MongooseRepo } from '../MongooseRepo';
 
 @Injectable()
-export class BlogsRepoService {
-  constructor(@InjectModel(Blog.name) private blogModel: Model<Blog>) { }
-
-  async save(createCatDto: CreateBlogDto): Promise<BlogDocument> {
-    const createdBlog = new this.blogModel(createCatDto);
-    return (await createdBlog.save());
-  }
-
-  async findById(id: string): Promise<BlogDocument | null> {
-    return await this.blogModel.findById(id);
-  }
-
-  async find(searchNameTerm?: string, skip: number = 0, limit: number = 10): Promise<BlogDocument[]> {
-    let searchPattern = searchNameTerm ? { name: searchNameTerm } : {};
-    return await this.blogModel.find(searchPattern).skip(skip).limit(limit);
-  }
-  async update(blog: BlogDocument){
-    return (await blog.save());
-  }
-
-  async count(searchNameTerm?: string) {
-    let searchPattern = searchNameTerm ? { name: searchNameTerm } : {};
-    return await this.blogModel.count(searchPattern);
-  }
-
-  async deleteById(id: string){
-    let deletedBlog = await this.blogModel.findByIdAndDelete(id);
-
-    return deletedBlog || null;
+export class BlogsRepoService extends MongooseRepo<Blog,CreateBlogDto ,BlogDocument> {
+  constructor(@InjectModel(Blog.name) private blogModel: Model<Blog>) { 
+    super(blogModel)
   }
 }
---
+++ src/Entities/Repos/MongooseRepo.ts
@@ -0,0 +1,33 @@
+import { HydratedDocument, Model } from "mongoose";
+
+export class MongooseRepo<ModelType, CreateDTO, EntityDocument extends HydratedDocument<ModelType>>{
+    constructor(private model: Model<ModelType>) { }
+
+  async save(createDTO: CreateDTO): Promise<EntityDocument> {
+    const createEntity = new this.model(createDTO);
+    return (await createEntity.save() as EntityDocument);
+  }
+
+  async findById(id: string): Promise<EntityDocument | null> {
+    return await this.model.findById(id);
+  }
+
+  async find(searchNameTerm?: string, skip: number = 0, limit: number = 10): Promise<EntityDocument[]> {
+    let searchPattern = searchNameTerm ? { name: searchNameTerm } : {};
+    return await this.model.find(searchPattern).skip(skip).limit(limit) as EntityDocument[];
+  }
+  async update(document: EntityDocument){
+    return (await document.save());
+  }
+
+  async count(searchNameTerm?: string) {
+    let searchPattern = searchNameTerm ? { name: searchNameTerm } : {};
+    return await this.model.count(searchPattern);
+  }
+
+  async deleteById(id: string): Promise<EntityDocument> | null {
+    let deletedDocument = await this.model.findByIdAndDelete(id) as EntityDocument;
+
+    return deletedDocument || null;
+  }
+}
