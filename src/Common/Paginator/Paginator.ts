256bc4643213801cc86e00d2fb9236eb36fb5994
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2023-10-25 03:12:24 +0200

    Поправил формирование входного paginator

--- src/Common/Paginator/Paginator.ts
+++ src/Common/Paginator/Paginator.ts
@@ -8,8 +8,8 @@ export class InputPaginator{
         pageSize_str: string | undefined,
         public sortDirection: "asc" | "desc" = "desc"
     ) {
-        this.pageNumber = parseInt(pageNumber_str) | 1;
-        this.pageSize = parseInt(pageSize_str) | 10;
+        this.pageNumber = parseInt(pageNumber_str) || 1;
+        this.pageSize = parseInt(pageSize_str) || 10;
         this.skipElements = (this.pageNumber - 1) * this.pageSize;
     }
 }
